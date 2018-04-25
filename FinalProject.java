39065e21570a6b5fda200259a3f0c2787f4b763e
Tar-Telperien <37634860+Tar-Telperien@users.noreply.github.com>
2018-04-24 20:57:43 -0500

    Update FinalProject.java
    Next running version. Still shows nothing and needs paint method.

--- FinalProject.java
+++ FinalProject.java
@@ -1,4 +1,5 @@
 /*Things needing to be done:
+2. Figure out how to create a bufferedImage in white as the background.
 3. Animate circles to follow mouse motion by creating a list of circles and delaying their motion by different amounts.
 4. Change circles to be parts of an animal
 5. Add 3D graphics
@@ -7,6 +8,11 @@ Things done:
 1. Populate HashMap
 2. Implement mouse movement sensor
 3. Create simple GUI interface.
+4. At the end of the method to change the visuals, use panel.repaint(); to tell Java that it needs to redraw itself.
+
+Take distance between current mouse location & current circle location
+Divide by # of circles
+Circle N moves dist - y*(n-1)
 */
 
 import javax.swing.JFrame;
@@ -20,48 +26,83 @@ import java.util.Scanner;
 import java.awt.MouseInfo;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 public class FinalProject extends JFrame {
+
+    //Constants:
+    static int Width = 1000;
+    static int Height = 700;
+    static int initX = Width/2;
+    static int initY = Height/2;
+    static int circleDiam = 50;
+    static int circleR = 25;
+    static Point currentPos = new Point(initX, initY);
 
     public static void main(String[] args){
         FinalProject frame = new FinalProject();
 
         frame.pack();
         frame.setVisible(true);
-
-        populateHashMap(500, 350, 10); //REPLACE MAGIC NUMBERS
     }
-
 
     //hashmap to hold the circles that we'll be animating
     static HashMap<String, Point> circles = new HashMap<String, Point>();
 
+//NOTE: write new paint method for the jpanel; use @override; tell that paint method to place the buffered image on the screen.
 
     //populateHashMap function
-    static void populateHashMap(int i, int j, int num){
+    //make each dist equal to the distance between the current circle position and the mouse position in the x or y direction, divided by the # of circles to be drawn.
+    static void populateHashMap(int i, int j, int xdist, int ydist, int num){
         int counter = 1;
+        int x = i;
+        int y = j;
         for(int m = 0; m < num; m++){
-            int x = i;
-            int y = j;
             Point Position = new Point(x, y);
             circles.put(Integer.toString(counter), Position);
             counter++;
+            x = x-xdist+circleR;
+            y=y-ydist+circleR;
         }
     }
 
     public FinalProject(){
         this.setLayout(new BorderLayout());
-        this.setPreferredSize(new Dimension(1000, 700));
+        this.setPreferredSize(new Dimension(Width, Height));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel main = new JPanel();
         main.setLayout(new BorderLayout());
+        Point p = new Point();
 
         main.addMouseListener(new MouseAdapter(){
             public void mouseMoved(MouseEvent e){
-                Point currentPos = MouseInfo.getPointerInfo().getLocation();
-                //Need a way to get currentPos as a global variable.
+                currentPos = MouseInfo.getPointerInfo().getLocation();
             }
         });
+
+        populateHashMap(currentPos.x, currentPos.y, 1, 1, 10);
+
+        BufferedImage base = buildImage();
+
+        //for entry in hashmap, draw an ellipse
+        for(String c:(circles.keySet()))
+            p = circles.get(c);
+            Graphics g = base.getGraphics();
+            Graphics2D g2 = (Graphics2D)g;
+            g2.draw(new Ellipse2D.Double(p.x, p.y, circleDiam, circleDiam));
+
+        main.repaint(); //redraws the panel at some rate
     }
+
+    BufferedImage buildImage(){
+	    BufferedImage im = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB);
+	    Graphics g = im.getGraphics();
+	    g.fillRect(0, 0, Width, Height);
+	    return im;
+    }
+
 }
