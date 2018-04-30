987f1bb4906fa7cfa22a5b806dd858ba5c19998e
Tar-Telperien <37634860+Tar-Telperien@users.noreply.github.com>
2018-04-30 14:51:56 -0500

    Remove all previous project specific code.

--- FinalProject.java
+++ FinalProject.java
@@ -1,20 +1,3 @@
-/*Things needing to be done:
-2. Figure out how to create a bufferedImage in white as the background.
-3. Animate circles to follow mouse motion by creating a list of circles and delaying their motion by different amounts.
-4. Change circles to be parts of an animal
-5. Add 3D graphics
-6. Figure out how to actually draw circles with a coordinate point as centre.
-Things done:
-1. Populate HashMap
-2. Implement mouse movement sensor
-3. Create simple GUI interface.
-4. At the end of the method to change the visuals, use panel.repaint(); to tell Java that it needs to redraw itself.
-
-Take distance between current mouse location & current circle location
-Divide by # of circles
-Circle N moves dist - y*(n-1)
-*/
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -47,25 +30,7 @@ public class FinalProject extends JFrame {
         new FinalProject();
     }
 
-    //hashmap to hold the circles that we'll be animating
-    static HashMap<String, Point> circles = new HashMap<String, Point>();
-
 //NOTE: write new paint method for the jpanel; use @override; tell that paint method to place the buffered image on the screen.
-
-    //populateHashMap function
-    //make each dist equal to the distance between the current circle position and the mouse position in the x or y direction, divided by the # of circles to be drawn.
-    static void populateHashMap(int i, int j, int xdist, int ydist, int num){
-        int counter = 1;
-        int x = i;
-        int y = j;
-        for(int m = 0; m < num; m++){
-            Point Position = new Point(x, y);
-            circles.put(Integer.toString(counter), Position);
-            counter++;
-	    x = x-xdist+circleR;
-	    y=y-ydist+circleR;
-	}
-    }
 
     BufferedImage base;
 
@@ -74,11 +39,12 @@ public class FinalProject extends JFrame {
         this.setPreferredSize(new Dimension(Width, Height));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	base = buildImage(BufferedImage.TYPE_INT_RGB);
+	    base = buildImage(BufferedImage.TYPE_INT_RGB);
 
         JPanel main = new JPanel(){
             public void paintComponent(Graphics g){
-                    super.paintComponent(g); //asks the parent class to do its thing so that setup and cleanup will be taken care of
+                    super.paintComponent(g); //asks the parent class to do its thing so that setup
+                    //and cleanup will be taken care of
                     g.drawImage(base, 0, 0, this);
                 }
         };
@@ -88,20 +54,11 @@ public class FinalProject extends JFrame {
         main.addMouseListener(new MouseAdapter(){
             public void mouseMoved(MouseEvent e){
                 currentPos = MouseInfo.getPointerInfo().getLocation();
+                p = currentPos;
             }
         });
 
-        populateHashMap(currentPos.x, currentPos.y, 1, 1, 10);
-
-        //for entry in hashmap, draw an ellipse
-        for(String c:(circles.keySet())){
-            p = circles.get(c);
-            Graphics g = base.getGraphics();
-            Graphics2D g2 = (Graphics2D)g;
-            g2.draw(new Ellipse2D.Double(p.x, p.y, circleDiam, circleDiam));
-        }
-
-	add(main, BorderLayout.CENTER);
+	    add(main, BorderLayout.CENTER);
 
         pack();
         setVisible(true);
@@ -113,6 +70,11 @@ public class FinalProject extends JFrame {
 	    BufferedImage im = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB);
 	    Graphics g = im.getGraphics();
 	    g.fillRect(0, 0, Width, Height);
+	    //now figure out a mask, or set of masks, based off of the distance from P.
 	    return im;
+    }
+
+    BufferedImage addFractal(BufferedImage img, int fracType, Point p){
+
     }
 }
